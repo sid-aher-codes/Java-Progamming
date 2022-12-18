157bc15b5c01ae9589f0729ea7e21a95ce89277b
sid-aher-codes <120816320+sid-aher-codes@users.noreply.github.com>
2022-12-17 20:24:48 -0500

    If and else in Java.
    An if-and-else program is written in Java.

--- If.java
+++ If.java
@@ -1,10 +1,16 @@
+// this program is an 'if and else statement' written in Java
+
+
+// a public class called 'Main'
 public class Main{
     public static void main(String[]args) {
-
+// integer number is 10
 int number = 10;
-
+// if 10 is greater than 5
 if (number > 5)
+// print, 10 is greater than 5
     System.out.println("10 is greater than 5");
+// or else, it will print, 10 is not greater than 5       
 else
     System.out.println("10 is not greater than 5");
    }
