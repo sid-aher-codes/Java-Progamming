4819bbcefe7082116be7a1f22fc0a8e2ce87345b
sid-aher-codes <120816320+sid-aher-codes@users.noreply.github.com>
2022-12-17 20:18:22 -0500

    If and else

---
+++ If.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[]args) {
+
+int number = 10;
+
+if (number > 5)
+    System.out.println("10 is greater than 5");
+else
+    System.out.println("10 is not greater than 5");
+   }
+}
