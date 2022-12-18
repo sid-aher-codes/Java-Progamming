ca9461c6a35e35a15c6da856ba09edd9bf8c2d5f
sid-aher-codes <120816320+sid-aher-codes@users.noreply.github.com>
2022-12-18 09:20:01 -0500

    Created a while loop in Java.

---
+++ While.java
@@ -0,0 +1,17 @@
+// This program is a while loop in Java
+
+// A public class named Main
+public class Main{
+    public static void main(String[] args) {
+        // integer value i is 10 and n is 5
+        int i = 1, n = 5;
+        //while 1 is less than 5
+        while(i <= n) {
+          // keep printing i until it gets to 5
+            System.out.println(i);'
+              // i should add 1 everytime until it gets to 5
+            i++;
+        }
+        
+    }
+}
